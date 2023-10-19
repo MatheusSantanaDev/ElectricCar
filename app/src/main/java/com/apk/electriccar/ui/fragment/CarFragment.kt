a51976a88459db5ccef1b20572a8b0a9353bf9f8
MatheusSantanaDev <matheus.santanadearaujo@outlook.com>
2023-10-19 20:35:07 -0300

    Refactor: Conexao com API por retrofit
    Codigo refatorado com o uso do retrofit, as requisoes http agora sao feitas atraves dele.

--- app/src/main/java/com/apk/electriccar/ui/fragment/CarFragment.kt
+++ app/src/main/java/com/apk/electriccar/ui/fragment/CarFragment.kt
@@ -4,16 +4,15 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.AsyncTask
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -23,15 +22,16 @@ import com.apk.electriccar.R.id.iv_no_wifi
 import com.apk.electriccar.R.id.pb_loading
 import com.apk.electriccar.R.id.rv_cars_list
 import com.apk.electriccar.R.id.tv_no_wifi
-import com.apk.electriccar.data.Garage
+import com.apk.electriccar.data.CarsApi
 import com.apk.electriccar.domain.Car
 import com.apk.electriccar.ui.AutonomyCalculateActivity
 import com.apk.electriccar.ui.adapter.CarAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import org.json.JSONArray
-import org.json.JSONTokener
-import java.net.HttpURLConnection
-import java.net.URL
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class CarFragment : Fragment() {
     private lateinit var fabGoCalculator: FloatingActionButton
@@ -39,6 +39,7 @@ class CarFragment : Fragment() {
     private lateinit var loading: ProgressBar
     private lateinit var noWifiImage: ImageView
     private lateinit var noWifiText: TextView
+    private lateinit var carsApi: CarsApi
 
     var carArray: ArrayList<Car> = ArrayList()
 
@@ -50,6 +51,7 @@ class CarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRetrofit()
         setupView(view)
         setupListeners()
     }
@@ -57,10 +59,42 @@ class CarFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         if(checkForInternet(context)){
-            callService()
+            getAllCars()
         }else{
             emptyState()
         }
+    }
+
+    private fun setupRetrofit(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://igorbag.github.io/cars-api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        carsApi = retrofit.create(CarsApi::class.java)
+    }
+
+    private fun getAllCars(){
+        carsApi.getAllCars().enqueue(object: Callback<List<Car>>{
+            override fun onResponse(call: Call<List<Car>>, response: Response<List<Car>>) {
+                if(response.isSuccessful){
+                    loading.isVisible = false
+                    noWifiImage.isVisible = false
+                    noWifiText.isVisible = false
+
+                    response.body()?.let {
+                        setupList(it)
+                    }
+                }else{
+                    Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Car>>, t: Throwable) {
+                Toast.makeText(context, R.string.response_error, Toast.LENGTH_LONG).show()
+            }
+
+        })
     }
 
     private fun emptyState(){
@@ -80,7 +114,7 @@ class CarFragment : Fragment() {
         }
     }
 
-    private fun setupList() {
+    private fun setupList(carArray: List<Car>) {
         val adapter = CarAdapter(carArray)
         carList.isVisible = true
         carList.adapter = adapter
@@ -90,12 +124,6 @@ class CarFragment : Fragment() {
         this.fabGoCalculator.setOnClickListener {
             startActivity(Intent(context, AutonomyCalculateActivity::class.java))
         }
-    }
-
-    private fun callService() {
-        val urlBase = "https://igorbag.github.io/cars-api/cars.json"
-        loading.isVisible = true
-        MyTask().execute(urlBase)
     }
 
     private fun checkForInternet(context: Context?): Boolean{
@@ -118,70 +146,4 @@ class CarFragment : Fragment() {
         }
     }
 
-    inner class MyTask : AsyncTask<String, String, String>() {
-        override fun onPreExecute() {
-            super.onPreExecute()
-        }
-
-        override fun doInBackground(vararg url: String?): String {
-            var urlConnection: HttpURLConnection? = null
-
-            try {
-                val urlBase = URL(url[0])
-
-                urlConnection = urlBase.openConnection() as HttpURLConnection
-                urlConnection.connectTimeout = 60000
-                urlConnection.readTimeout = 60000
-                urlConnection.setRequestProperty(
-                    "accept", "application/json"
-                )
-                val responseCode = urlConnection.responseCode
-
-                if (responseCode == HttpURLConnection.HTTP_OK) {
-                    var response = urlConnection.inputStream.bufferedReader().use {
-                        it.readText()
-                    }
-                    publishProgress(response)
-                }else{
-                    Log.e("Erro", "Service currently unavaible...")
-                }
-            } catch (ex: Exception) {
-                Log.e("Erro", "Error when processing...")
-            } finally {
-                urlConnection?.disconnect()
-            }
-            return ""
-        }
-
-        override fun onProgressUpdate(vararg values: String?) {
-            try {
-                val jsonArray = JSONTokener(values[0]).nextValue() as JSONArray
-
-                for (i in 0 until jsonArray.length()) {
-                    val id = jsonArray.getJSONObject(i).getInt("id")
-                    val price = jsonArray.getJSONObject(i).getString("preco")
-                    val battery = jsonArray.getJSONObject(i).getString("bateria")
-                    val power = jsonArray.getJSONObject(i).getString("potencia")
-                    val recharge = jsonArray.getJSONObject(i).getString("recarga")
-                    val urlImage = jsonArray.getJSONObject(i).getString("urlPhoto")
-
-                    val model = Car(
-                        id = id,
-                        price = price,
-                        battery = battery,
-                        power = power,
-                        recharge = recharge,
-                        urlImage = urlImage
-                    )
-                    carArray.add(model)
-                }
-                loading.isVisible = false
-                noWifiImage.isVisible = false
-                noWifiText.isVisible = false
-                setupList()
-            } catch (ex: Exception) {
-                Log.e("Erro ->", ex.message.toString())
-            }
-        }
-    }
 }
