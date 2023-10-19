0c1308e837369c7d99fd9931332d32695d64ed35
MatheusSantanaDev <matheus.santanadearaujo@outlook.com>
2023-10-19 19:54:59 -0300

    Feat: Verificaçao conexao com a internet
    Adicionado mensagem caso esteja sem conexao e efeito de carregamento com a api garagem.

--- app/src/main/java/com/apk/electriccar/ui/fragment/CarFragment.kt
+++ app/src/main/java/com/apk/electriccar/ui/fragment/CarFragment.kt
@@ -1,17 +1,28 @@
 package com.apk.electriccar.ui.fragment
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.apk.electriccar.R
 import com.apk.electriccar.R.id.fab_calculator
+import com.apk.electriccar.R.id.iv_no_wifi
+import com.apk.electriccar.R.id.pb_loading
 import com.apk.electriccar.R.id.rv_cars_list
+import com.apk.electriccar.R.id.tv_no_wifi
 import com.apk.electriccar.data.Garage
 import com.apk.electriccar.domain.Car
 import com.apk.electriccar.ui.AutonomyCalculateActivity
@@ -25,6 +36,9 @@ import java.net.URL
 class CarFragment : Fragment() {
     private lateinit var fabGoCalculator: FloatingActionButton
     private lateinit var carList: RecyclerView
+    private lateinit var loading: ProgressBar
+    private lateinit var noWifiImage: ImageView
+    private lateinit var noWifiText: TextView
 
     var carArray: ArrayList<Car> = ArrayList()
 
@@ -36,20 +50,39 @@ class CarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        callService()
         setupView(view)
         setupListeners()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(checkForInternet(context)){
+            callService()
+        }else{
+            emptyState()
+        }
+    }
+
+    private fun emptyState(){
+        loading.isVisible = false
+        carList.isVisible = false
+        noWifiImage.isVisible = true
+        noWifiText.isVisible = true
     }
 
     private fun setupView(view: View) {
         view.apply {
             fabGoCalculator = findViewById(fab_calculator)
             carList = findViewById(rv_cars_list)
+            loading = findViewById(pb_loading)
+            noWifiText = findViewById(tv_no_wifi)
+            noWifiImage = findViewById(iv_no_wifi)
         }
     }
 
     private fun setupList() {
         val adapter = CarAdapter(carArray)
+        carList.isVisible = true
         carList.adapter = adapter
     }
 
@@ -61,7 +94,28 @@ class CarFragment : Fragment() {
 
     private fun callService() {
         val urlBase = "https://igorbag.github.io/cars-api/cars.json"
+        loading.isVisible = true
         MyTask().execute(urlBase)
+    }
+
+    private fun checkForInternet(context: Context?): Boolean{
+        val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork ?: return false
+            val activityNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when{
+                activityNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activityNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else{
+            @Suppress("DEPRECATION")
+            val networkInfo = connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 
     inner class MyTask : AsyncTask<String, String, String>() {
@@ -121,6 +175,9 @@ class CarFragment : Fragment() {
                     )
                     carArray.add(model)
                 }
+                loading.isVisible = false
+                noWifiImage.isVisible = false
+                noWifiText.isVisible = false
                 setupList()
             } catch (ex: Exception) {
                 Log.e("Erro ->", ex.message.toString())
